9ce993db8d207add71c217bd69606a157f03950e
tylerhipke <tyerhipke@gmail.com>
2019-09-14 11:21:21 -0700

    revert mysql

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var path = require("path");
 
+//define server variables-----------------------------------------
 var app = express();
+// var PORT = 3000;
 var PORT = process.env.PORT;
 
 app.use(express.urlencoded({
@@ -9,6 +11,7 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
+//html modifiers--------------------------------------------------
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"))
 });
@@ -21,10 +24,25 @@ app.get("/reservations", function (req, res) {
     res.sendFile(path.join(__dirname, "reservations.html"))
 });
 
+//pull api data as json-------------------------------------------
 app.get("/api/reservation", function (req, res) {
     return res.json(reservation);
 });
 
+//create new reservation as json----------------------------------
+app.post("/api/reservation", function(req, res) {
+    var newReservation = req.body;
+  
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newReservation);
+  
+    characters.push(newReservation);
+  
+    res.json(newReservation);
+  });
+
+//initialize host service-----------------------------------------
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
