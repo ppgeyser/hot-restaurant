ec00dd9f590cb2b51b7e0681eb163e4eb04e8118
tylerhipke <tyerhipke@gmail.com>
2019-09-14 11:30:43 -0700

    add sample reservation data

--- server.js
+++ server.js
@@ -11,6 +11,38 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
+//reservation object----------------------------------------------
+
+var reservations = [{
+        customerName: "Ahmed",
+        customerEmail: "afhaque89@gmail.com",
+        phoneNumber: "979-587-0887",
+        customerID: "afhaque89"
+    }, {
+        customerName: "Buddy E Toups",
+        customerEmail: "mydesignbuddy@gmail.com",
+        phoneNumber: "7138254601",
+        customerID: "BUDDYT"
+    }, {
+        customerName: "Tierra",
+        customerEmail: "trafarris@gmail.com",
+        phoneNumber: "7204381472",
+        customerID: "^__^"
+    },
+    {
+        customerName: "sparta",
+        customerEmail: "kickemtothepit@gmail.co",
+        phoneNumber: "300-300-3000",
+        customerID: "r3030"
+    },
+    {
+        customerName: "Drew",
+        customerEmail: "d@noemail.com",
+        phoneNumber: "11111111111111",
+        customerID: "sfd23"
+    }
+];
+
 //html modifiers--------------------------------------------------
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"))
@@ -30,20 +62,19 @@ app.get("/api/reservation", function (req, res) {
 });
 
 //create new reservation as json----------------------------------
-app.post("/api/reservation", function(req, res) {
+app.post("/api/reservation", function (req, res) {
     var newReservation = req.body;
-  
+
     newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
-  
+
     console.log(newReservation);
-  
-    reservation.push(newReservation);
-  
+
+    reservations.push(newReservation);
+
     res.json(newReservation);
-  });
+});
 
 //initialize host service-----------------------------------------
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
-
