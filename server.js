ba4be2a56314e1444cbfa83f35542bd1aa0072a9
tylerhipke <tyerhipke@gmail.com>
2019-09-14 11:04:08 -0700

    add package.json and add basic POST code

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 var express = require("express");
 var path = require("path");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
-app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname, "index.html"))
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"))
 });
 
-app.get("/tables", function(req,res){
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"))
 });
 
-app.get("/reservations", function(req,res){
+app.get("/reservations", function (req, res) {
     res.sendFile(path.join(__dirname, "reservations.html"))
 });
+
+app.get("/api/reservation", function (req, res) {
+    return res.json(reservation);
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
