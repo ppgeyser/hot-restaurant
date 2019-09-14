8a389b92ea06da2a2bcc5e3c9ee20dbc2c7cdc0c
Peter Geyser <peterpgeyser@gmail.com>
2019-09-14 12:45:22 -0700

    Created ajax POST javascript command

---
+++ reservations.js
@@ -0,0 +1,16 @@
+// Question: What does this code do?
+$("#add-reservation-btn").on("click", function(event) {
+    event.preventDefault();
+    var name = $("#name-input").val().trim();
+    var phone = $("#phone-input").val().trim();
+    var email =  $("#email-input").val().trim();
+    var uniqueID = $("#unique-ID").val().trim();
+    
+    var query = "api/reservation?" + name + "&" + email + "&" + phone + "&" + uniqueID
+
+    $.post(query)
+      .then(function(data) {
+        console.log("reservation.html", data);
+        alert("Adding reservation...");
+      });
+  });
